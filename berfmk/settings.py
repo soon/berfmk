fb8d6f2872227e36985c4f8de298d40576ad99c5
soon <0coming.soon@gmail.com>
2012-06-09 11:46:23 +0600

    Fixed ie9.0 block

--- berfmk/settings.py
+++ berfmk/settings.py
@@ -34,7 +34,7 @@ TIME_ZONE = 'Asia/Yekaterinburg'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en_en'
 
 SITE_ID = 1
 
